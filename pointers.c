4db24394b0f313a0a001b414590f606272527fb9
toxicfrank <ngugifrancis930@gmail.com>
2023-04-03 18:46:51 +0100

    practice

---
+++ pointers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - changing the variable
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int x = 6;
+	int *ptr;
+	ptr = &x;
+
+	printf("%d\n", x);
+
+	*ptr = 7;
+
+	printf("%d\n", *ptr);
+
+	printf("%p\n", &ptr);
+
+	printf("%d\n", x);
+
+	return (0);
+}
