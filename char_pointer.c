8a46fb350def3336774a7e4e9ce33f40ee4dd062
toxicfrank <ngugifrancis930@gmail.com>
2023-04-07 05:26:15 +0100

    practice

--- char_pointer.c
+++ char_pointer.c
@@ -1,1 +1,13 @@
 #include <stdio.h>
+/**
+ * main - still practicing pointers
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char *str = "Hello world!";/*Declare a character
+	pointer and initialize it to point to the string
+	"Hello world!"*/
+	printf("%s\n", str);/*Output: Hello world*/
+}
