63dcba0370eebdd63381624ba3a5d7cc98e767a4
toxicfrank <ngugifrancis930@gmail.com>
2023-04-07 05:15:28 +0100

    practice

---
+++ character.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - learing pointers still
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char ch = 'A';
+	char *ptr = &ch;
+
+	printf("The value of ch is %c\n", ch);/*output:
+	 The value of ch is A*/
+	printf("The value of ptr is %p\n", ptr);/*output:
+	 The value of ptr is the memory address of ch*/
+	printf("The value of *ptr is is %c\n", *ptr);/*output:
+	 The value of *ptr is the value of ch*/
+
+	*ptr = 'B';
+	printf("The value of ch is now %c\n", ch);/*output: 
+	The value of ch is now B*/
+
+	return (0);
+}
