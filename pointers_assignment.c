86e0bdb0fece9730377ec6becfec46fcb106cf3c
toxicfrank <ngugifrancis930@gmail.com>
2023-04-11 11:04:52 +0100

    practice

--- pointers_assignment.c
+++ pointers_assignment.c
@@ -7,17 +7,21 @@ int main(void)
 {
 	int i = 10;
 	int *p, *q;
+
 	p = &i;
+
 	q = p;
 
 	printf("%d %d \n", *p, *q);
 	/*Note that *q = *p */
-	
+
 	/*another example*/
 	int j = 1;
 	int *l = &j;
 	int *k;
+
 	k = l;
+
 	*k = 5;
 
 	printf("%d\n", *l);
