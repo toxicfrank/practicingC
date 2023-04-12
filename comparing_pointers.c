f5ba1d64bd88e3abcfeb8954faf5082a7d23f77c
toxicfrank <ngugifrancis930@gmail.com>
2023-04-12 19:35:24 +0100

    practice

---
+++ comparing_pointers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - comparing pointers
+ * Retturn: always 0
+ */
+int main(void)
+{
+	int a[] = {5, 16, 7, 89, 45, 32, 23, 10};
+	int *p = &a[1], *q = &a[5];
+
+	printf("%d\n", *(p + 3));
+	printf("%d\n", *(q - 3));
+	printf("%d\n", q - p);
+	printf("%d\n", p < q);
+	printf("%d\n", *p < *q);
+
+	return (0);
+}
